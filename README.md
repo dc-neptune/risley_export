b2b6c705eae1f589ec76c2fe9972133798ff0eab
Ben Neptune <neptune@dgcircus.com>
2024-02-22 10:58:48 +0900

    Fixed to run without installing mod

--- README.md
+++ README.md
@@ -10,3 +10,14 @@
 3. `drush scr docroot/modules/custom/risley_export/scripts/export.php`を実行します。
 
 初めて呼び出す際には、かなりのメモリが使用されます。Localization.xlsをダウンロードしない場合、メモリ制限を超える可能性があります。成功するまで実行し続けるだけで十分です。
+
+注意点：
+場合によって、サイトのcomposer.jsonがdrupal-custom-moduleのインストールパスが定義してない場合もあります。なければ、追加する必要があります。
+```
+"installer-paths": [
+    // ...他のパス
+    "docroot/modules/custom/{$name}": [
+        "type:drupal-custom-module"
+    ],
+]
+```
--- scripts/export.php
+++ scripts/export.php
@@ -6,10 +6,27 @@
  */
 
 use Drupal\risley_export\Commands\RisleyExportCommands;
+use Drupal\risley_export\Sheets\Factory\BaseSheetFactory;
 
-$autoloader = require_once 'autoload.php';
-require_once __DIR__ . '/../src/Commands/RisleyExportCommands.php';
-$command = new RisleyExportCommands(
-    \Drupal::service('risley_export.base_sheet_factory')
+require_once 'autoload.php';
+require_once DRUPAL_ROOT . '/modules/custom/risley_export/src/Commands/RisleyExportCommands.php';
+require_once DRUPAL_ROOT . '/modules/custom/risley_export/src/Sheets/Factory/BaseSheetFactory.php';
+
+// Create instances of the required services.
+$baseSheetFactory = new BaseSheetFactory(
+    \Drupal::service('entity_field.manager'),
+    \Drupal::service('entity_type.manager'),
+    \Drupal::service('plugin.manager.field.field_type'),
+    \Drupal::service('language_manager'),
+    \Drupal::service('config.factory'),
+    \Drupal::service('entity_type.bundle.info'),
+    \Drupal::service('entity.repository'),
+    \Drupal::service('extension.list.module'),
+    \Drupal::service('user.permissions'),
+    \Drupal::service('logger.factory'),
+    \Drupal::service('info_parser')
 );
+
+// Create an instance of your command class with the required services.
+$command = new RisleyExportCommands($baseSheetFactory);
 $command->exportSheetData();
