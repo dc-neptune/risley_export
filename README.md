37d7ccf4cda52a4ab9b061a5eaa3d8138f50c207
Ben Neptune <neptune@dgcircus.com>
2024-02-22 10:10:49 +0900

    Fixed script

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-使い方
+モジュールの使い方
 
 1. Localization.xlsをDropboxからダウンロードし、Localization/フォルダに保存します。
 2. モジュールを有効にします。
-3. `make drush re:e` を実行します。
+3. `make drush re:e`を実行します。
+
+スクリプトの使い方：
+1. Localization.xlsをDropboxからダウンロードし、Localization/フォルダに保存します。
+2. `make sh php`を実行します。
+3. `drush scr docroot/modules/custom/risley_export/scripts/export.php`を実行します。
 
 初めて呼び出す際には、かなりのメモリが使用されます。Localization.xlsをダウンロードしない場合、メモリ制限を超える可能性があります。成功するまで実行し続けるだけで十分です。
--- scripts/export.php
+++ scripts/export.php
@@ -5,41 +5,11 @@
  * A script to programmatically export drupalsettings from a script.
  */
 
-use DrupalFinder\DrupalFinder;
-use Drush\Drush;
+use Drupal\risley_export\Commands\RisleyExportCommands;
 
-// Find and load Drupal.
-require_once __DIR__ . '/vendor/autoload.php';
-
-$finder = new DrupalFinder();
-$finder->locateRoot(__DIR__);
-
-if (!$finder->locateRoot()) {
-    throw new \RuntimeException('Drupal root not found. Please check your setup.');
-}
-
-// Set up the site path and bootstrap Drupal.
-$drupal_root = $finder->getDrupalRoot();
-$site_path = $finder->getSitePath();
-
-// Bootstrap Drupal.
-define('DRUPAL_ROOT', $drupal_root);
-require_once DRUPAL_ROOT . '/core/includes/bootstrap.inc';
-drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-
-// Load necessary Drupal components.
-\Drupal::service('kernel')->boot();
-
-// Initialize Drush.
-Drush::bootstrap();
-
-// Load your Drush command class.
-require_once __DIR__ . '/modules/custom/risley_export/Commands/RisleyExportCommands.php';
-
-// Create an instance of your command class.
-$command = new \Drupal\risley_export\Commands\RisleyExportCommands(
-    \Drupal::service('risley_export.sheet_factory')
+$autoloader = require_once 'autoload.php';
+require_once __DIR__ . '/../src/Commands/RisleyExportCommands.php';
+$command = new RisleyExportCommands(
+    \Drupal::service('risley_export.base_sheet_factory')
 );
-
-// Execute the method from your command class.
 $command->exportSheetData();
