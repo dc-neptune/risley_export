3571ec69cb562bbc9640b51e4924e6f75bc7df25
Ben Neptune <neptune@dgcircus.com>
2024-03-11 17:01:32 +0900

    phpcs

--- scripts/export.php
+++ scripts/export.php
@@ -14,24 +14,25 @@ require_once DRUPAL_ROOT . '/modules/custom/risley_export/src/Sheets/Factory/Bas
 
 // Create instances of the required services.
 $baseSheetFactory = new BaseSheetFactory(
-    \Drupal::service('entity_field.manager'),
-    \Drupal::service('entity_type.manager'),
-    \Drupal::service('plugin.manager.field.field_type'),
-    \Drupal::service('language_manager'),
-    \Drupal::service('config.factory'),
-    \Drupal::service('entity_type.bundle.info'),
-    \Drupal::service('entity.repository'),
-    \Drupal::service('extension.list.module'),
-    \Drupal::service('user.permissions'),
-    \Drupal::service('logger.factory'),
-    \Drupal::service('info_parser')
+  \Drupal::service('entity_field.manager'),
+  \Drupal::service('entity_type.manager'),
+  \Drupal::service('plugin.manager.field.field_type'),
+  \Drupal::service('language_manager'),
+  \Drupal::service('config.factory'),
+  \Drupal::service('entity_type.bundle.info'),
+  \Drupal::service('entity.repository'),
+  \Drupal::service('extension.list.module'),
+  \Drupal::service('user.permissions'),
+  \Drupal::service('logger.factory'),
+  \Drupal::service('info_parser')
 );
 
 // Create an instance of your command class with the required services.
 // @todo Figure out why exceptions and loggers are not getting passed
 try {
-    $command = new RisleyExportCommands($baseSheetFactory);
-    $command->exportSheetData();
-} catch (\Exception $e) {
-    echo 'Exception caught: ',  $e->getMessage(), "\n";
+  $command = new RisleyExportCommands($baseSheetFactory);
+  $command->exportSheetData();
+}
+catch (\Exception $e) {
+  echo 'Exception caught: ', $e->getMessage(), "\n";
 }
